3d12778e29004fab2c04c4077bc7edb95de05556
guanjun <guanjun@itiger.com>
2019-11-16 01:36:12 +0800

    MultipartResolver 文件解析和延时策略

--- spring-webmvc/src/main/java/org/springframework/web/servlet/DispatcherServlet.java
+++ spring-webmvc/src/main/java/org/springframework/web/servlet/DispatcherServlet.java
@@ -1008,6 +1008,7 @@ public class DispatcherServlet extends FrameworkServlet {
 			Exception dispatchException = null;
 
 			try {
+				//设计文件大小和数目校验
 				processedRequest = checkMultipart(request);
 				multipartRequestParsed = (processedRequest != request);
 
