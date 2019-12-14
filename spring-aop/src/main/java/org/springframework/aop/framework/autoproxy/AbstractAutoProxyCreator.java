6621ff419827d697acbd76f1c0408634c40e1b2d
guanjun <guanjun@itiger.com>
2019-12-14 23:29:38 +0800

    ProxyCreatorSupport 管理Advisors信息

--- spring-aop/src/main/java/org/springframework/aop/framework/autoproxy/AbstractAutoProxyCreator.java
+++ spring-aop/src/main/java/org/springframework/aop/framework/autoproxy/AbstractAutoProxyCreator.java
@@ -346,7 +346,7 @@ public abstract class AbstractAutoProxyCreator extends ProxyProcessorSupport
 			this.advisedBeans.put(cacheKey, Boolean.FALSE);
 			return bean;
 		}
-		//Spring AOP框架默认用单例SingletonTargetSource封装Bean实例
+		//Spring AOP框架默认用SingletonTargetSource封装Bean实例
 		// Create proxy if we have advice.
 		Object[] specificInterceptors = getAdvicesAndAdvisorsForBean(bean.getClass(), beanName, null);
 		if (specificInterceptors != DO_NOT_PROXY) {
